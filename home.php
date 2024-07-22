a08a517e06c4ea2a329682772d66bb22846757d8
chonticha <96102341+cc-chonticha@users.noreply.github.com>
2024-07-23 00:09:16 +0700

    final

--- home.php
+++ home.php
@@ -5,15 +5,29 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>HOME PAGE</title>
-
+        <link rel="stylesheet" href="style.css">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
             integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     </head>
 
     <body>
         <div class="container">
-            <h1>Go to Insert</h1>
-            <a href="insert.php" class="btn btn-success">Go to insert</a>
+            <div class="login-container">
+                <h2>Login</h2>
+                <form action="login.php" method="post">
+                    <div class="form-group">
+                        <label for="username">Username</label>
+                        <input type="text" id="username" name="username" required>
+                    </div>
+                    <div class="form-group">
+                        <label for="password">Password</label>
+                        <input type="password" id="password" name="password" required>
+                    </div>
+                    <a href="insert.php" class="btn btn-success">Login</a>
+                </form>
+            </div>
+
+
 
         </div>
 
